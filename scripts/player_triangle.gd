62ba9b051f2018d3a00a490d8b99816ecc68d0bd
Eric Flamm <ericwflamm@gmail.com>
2025-11-17 11:14:35 -0700

    demo map, added double jump, prototype jump rotation

--- scripts/player_triangle.gd
+++ scripts/player_triangle.gd
@@ -5,6 +5,8 @@ var slow_acceleration = 80
 var jump_speed = -speed*2
 var gravity = speed * 5
 var max_grav_speed = 100
+var extra_jump = true
+var jump_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,6 +21,11 @@ func _physics_process(delta: float) -> void:
 	handle_input()
 	move_and_slide()
 	update_movement(delta)
+	if !is_on_floor():
+		rotation += 0.083
+	else:
+		rotation = 0
+		extra_jump = true
 	
 	
 
@@ -26,8 +33,13 @@ func update_movement(delta: float) -> void:
 	velocity.y += gravity*delta
 
 func handle_input() -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || extra_jump):
 		velocity.y = jump_speed
+		jump_count += 1
+		print(jump_count)
+		if jump_count > 1:
+			extra_jump = false
+			jump_count = 0
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction == 0:
