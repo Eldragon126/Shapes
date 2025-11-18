37301480c908418f8dd948963db410ce342b7dfa
Eric Flamm <ericwflamm@gmail.com>
2025-11-18 14:44:04 -0700

    changes

--- scripts/player_triangle.gd
+++ scripts/player_triangle.gd
@@ -8,11 +8,12 @@ var max_grav_speed = 100
 var extra_jump = true
 var jump_count = 0
 var projectile_speed: float = 1000
+var health = 100
 @onready var lobber_projectile = preload("res://nodes/lobber_projectile.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	PlayerManager.triangle_player = self
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,9 +21,10 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
+	update_movement(delta)
 	handle_input()
 	move_and_slide()
-	update_movement(delta)
+	take_damage_death()
 	#handle_attack()
 
 func update_movement(delta: float) -> void:
@@ -41,12 +43,15 @@ func handle_input() -> void:
 		velocity.x = move_toward(velocity.x,0,slow_acceleration)
 	else:
 		velocity.x = move_toward(velocity.x,speed* direction, acceleration)
-	
 	if !is_on_floor():
 		rotation += (0.083 * direction)
 	else:
 		rotation = 0
 		extra_jump = true
+
+func take_damage_death():
+	if health <= 0:
+		queue_free()
 
 #func handle_attack():
 #	if Input.is_action_just_pressed("attack"):
