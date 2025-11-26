7bfcdcda38f1be00381f55b7d46437a0dd76804b
Eric Flamm <ericwflamm@gmail.com>
2025-11-26 12:39:55 -0700

    functional lever

--- scripts/lever.gd
+++ scripts/lever.gd
@@ -17,3 +17,17 @@ func _process(delta: float) -> void:
 		$AnimationPlayer.play("LeverPull")
 		await $AnimationPlayer.animation_finished
 		off = false
+
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("touched Lever")
+	if off == false:
+		$AnimationPlayer.play("LeverOff")
+		await $AnimationPlayer.animation_finished
+		off = true
+	else:
+		$AnimationPlayer.play("LeverPull")
+		await $AnimationPlayer.animation_finished
+		off = false
