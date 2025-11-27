e6a5e87bfece5a76084d8a2b12c142efa3f785ee
Eric Flamm <ericwflamm@gmail.com>
2025-11-27 10:06:43 -0700

    commit

--- scripts/ballon_transition.gd
+++ scripts/ballon_transition.gd
@@ -4,6 +4,8 @@ var player: Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = PlayerManager.player
+	print("The balloons position is: " + str(position))
+	#position=player.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -11,9 +13,7 @@ func _process(delta: float) -> void:
 		if player and Input.is_action_just_pressed("interact_button"):
 			$TextureButton.show()
 			PlayerManager.game_paused = true
-			
 			Engine.time_scale = 0
-			
 	else:
 		$TextureButton.hide()
 		
@@ -27,4 +27,13 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_floor_3_pressed() -> void:
 	print("thrid floor")
-	$AnimationPlayer.play("ballon_ui")
+	#$AnimationPlayer.play("ballon_ui")
+	var ballon_down = get_tree().create_tween()
+	var player_down = get_tree().create_tween()
+	ballon_down.set_ignore_time_scale(true)
+	player_down.set_ignore_time_scale(true)
+	#player.get_node("./player_cam").enabled = false
+	ballon_down.tween_property($Sprite2D, "scale", Vector2(2, 2), 2.0)
+	ballon_down.parallel().tween_property(self, "position:y", self.position.y + 800, 6.0)
+	player_down.parallel().tween_property(player, "position:y", player.position.y + 800, 6.0)
+	ballon_down.parallel().tween_property($player_cam, "position:y", player.position.y + 800, 6.0)
