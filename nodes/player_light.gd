425eb9d445a10b1df0720d65e71d340ecabfc588
Eldragon126 <eldensimmons1@gmail.com>
2025-11-29 19:35:07 -0700

    Light works a little better

--- nodes/player_light.gd
+++ nodes/player_light.gd
@@ -1,45 +1,47 @@
 extends Node2D
 @export var player: Node2D
-
+@export var max_speed = 3000
 func _ready() -> void:
 	player = PlayerManager.player
 
 func _physics_process(delta: float) -> void:
-	if abs(player.global_position - $".".global_position) < Vector2(10,300) && player.global_position != null:
-		$".".linear_velocity = $".".linear_velocity/60#Vector2(0,0)
+	$".".linear_velocity = $".".linear_velocity.limit_length(max_speed)
+	if abs(player.global_position - $".".global_position) < Vector2(100,50) && player.global_position != null:
+		$".".linear_velocity = $".".linear_velocity/1.5#Vector2(0,0)
+	if abs(player.global_position - $".".global_position) < Vector2(30,50) && player.global_position != null:
+		$".".linear_velocity = $".".linear_velocity/2#Vector2(0,0)
 		print("The force of the player light should be less")
 	elif abs(player.global_position - $".".global_position) < Vector2(1000,1000) && player.global_position != null:
 		print("adding force to player light")
-		if player.global_position.y < $".".global_position.y + 200:
+		if player.global_position.y < $".".global_position.y + 100:
 			if player.global_position.x < $".".global_position.x:
-				$".".apply_force(Vector2(-300, -300))
+				$".".apply_force(Vector2(-600, -600))
 				
 				#$".".linear_velocity = Vector2(-500, -100)
 			else:
-				$".".apply_force(Vector2(300, -300))
+				$".".apply_force(Vector2(600, -600))
 				#$".".linear_velocity = Vector2(500, -100)
 		else:
 			if player.global_position.x < $".".global_position.x:
-				$".".apply_force(Vector2(-300, 300))
+				$".".apply_force(Vector2(-600, 600))
 				#$".".linear_velocity = Vector2(-500, 100)
 			else:
-				$".".apply_force(Vector2(300, 300))
+				$".".apply_force(Vector2(600, 600))
 				#$".".linear_velocity = Vector2(500, 100)
 	else:
 		print("adding linear velocity to player light")
-		if player.global_position.y < $".".global_position.y + 200:
+		if player.global_position.y < $".".global_position.y + 100:
 			if player.global_position.x < $".".global_position.x:
 				#$".".apply_force(Vector2(-100, -10))
-				$".".linear_velocity = Vector2(-1000, -1000)
+				$".".linear_velocity = Vector2(-3000, -700)
 			else:
 				#$".".apply_force(Vector2(100, -10))
-				$".".linear_velocity = Vector2(1000, -1000)
+				$".".linear_velocity = Vector2(3000, -700)
 		else:
 			if player.global_position.x < $".".global_position.x:
 				#$".".apply_force(Vector2(-100, 10))
-				$".".linear_velocity = Vector2(-1000, 1000)
+				$".".linear_velocity = Vector2(-3000, 700)
 				
 			else:
 				#$".".apply_force(Vector2(100, 10))
-				$".".linear_velocity = Vector2(1000, 1000)
-	
+				$".".linear_velocity = Vector2(3000, 700)
