abc85e97ac7058c91f1b45752edfc39849c68440
Eldragon126 <eldensimmons1@gmail.com>
2025-12-15 16:01:21 -0700

    Made a change

--- scripts/disable_everything_of_player.gd
+++ scripts/disable_everything_of_player.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player.disabled_everything = true
+		player.extra_jump = false
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
