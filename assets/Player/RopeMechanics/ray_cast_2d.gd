571744216e36d7fac4330912f41521ac99d4b50d
Eldragon126 <eldensimmons1@gmail.com>
2025-12-15 12:19:18 -0700

    I'm just pushing my changes fast so hopefully it doesn't break

--- assets/Player/RopeMechanics/ray_cast_2d.gd
+++ assets/Player/RopeMechanics/ray_cast_2d.gd
@@ -5,5 +5,7 @@ func _draw() -> void:
 	draw_circle(to_local(get_collision_point()), 8 * float(not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)), Color(0.824, 0.824, 0.824, 1.0))
 
 func _process(delta: float) -> void:
-	if is_colliding():
+	if is_colliding() and PlayerManager.sides_player == 5:
 		queue_redraw()
+	else:
+		pass
